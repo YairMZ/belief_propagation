026cd539bcef067a7a397a63f423251663645067
Yair Mazal <yairmazal@gmail.com>
2021-11-18 22:25:10 +0200

    Add update rules to nodes

--- belief_propagation/node.py
+++ belief_propagation/node.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 import numpy as np
 import itertools
+from typing import Any, Callable
 
 
 class Node:
@@ -12,11 +13,12 @@ class Node:
         """
         self.name = name
         self.uid = next(Node._uid_generator)
-        self.neighbors: list[Node] = []
+        self.neighbors: dict[int, Node] = {}
+        self.received_messages: dict[int, Any] = {}  # keys as senders, values as messages
         self.likelihood = None
 
     def register_neighbor(self, neighbor: Node) -> None:
-        self.neighbors.append(neighbor)
+        self.neighbors[neighbor.uid] = neighbor
 
     def __str__(self) -> str:
         if self.name:
@@ -25,12 +27,13 @@ class Node:
             return str(self.uid)
 
     def get_neighbors(self) -> list[int]:
-        return [neighbor.uid for neighbor in self.neighbors]
+        return list(self.neighbors.keys())
 
     def receive_messages(self) -> None:
-        pass
+        for node_id, node in self.neighbors.items():
+            self.received_messages[node_id] = node.message(self.uid)
 
-    def send_message(self, neighbor_uid: int):
+    def message(self, requester_uid: int) -> Any:
         pass
 
     def __hash__(self):
@@ -39,10 +42,36 @@ class Node:
 
 class CNode(Node):
 
-    def update_llr(self):
-        pass
+    def initialize(self):
+        self.received_messages = {node_uid: 0 for node_uid in self.neighbors}
+
+    def message(self, requester_uid: int) -> np.float_:
+        def phi(x):
+            return -np.log(np.tanh(x/2))
+        q = np.array([msg for uid, msg in self.received_messages.items() if uid != requester_uid])
+        return np.prod(np.sign(q))*phi(np.sum(phi(q)))
 
 
 class VNode(Node):
-    def update_llr(self):
-        pass
+    def __init__(self, channel_model: Callable,name: str = ""):
+        """
+        :param channel_model: a function which receives channel outputs anr returns relevant message
+        :param name: optional name of node
+        """
+        self.channel_model = channel_model
+        self.channel_symbol: int = None  # currently assuming hard channel symbols
+        self.channel_llr: np.float_ = None
+        super().__init__(name)
+
+    def initialize(self, channel_symbol):
+        self.channel_symbol = channel_symbol
+        self.channel_llr = self.channel_model(channel_symbol)
+        self.received_messages = {node_uid: 0 for node_uid in self.neighbors}
+
+    def message(self, requester_uid: int) -> np.float_:
+        return self.channel_llr + np.sum(
+            [msg for uid, msg in self.received_messages.items() if uid != requester_uid]
+        )
+
+    def llr(self) -> np.float_:
+        return self.channel_llr + np.sum(list(self.received_messages.values()))
